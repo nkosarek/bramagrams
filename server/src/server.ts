cd47883127ff09a395acf28d3fd56e7d7a81260b
Nicholas Kosarek <nkosarek379@gmail.com>
2020-07-18 20:37:37 -0700

    Add dummy server code

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,20 @@
-console.log("TEST");
+import express from 'express';
+import http from 'http';
+import socketIO from 'socket.io';
+
+let app = express();
+let server = new http.Server(app);
+let io = socketIO(server);
+
+app.set('port', 5000);
+app.use('/static', express.static(__dirname + '/static'));
+
+// Routing
+app.get('/', function(request: any, response: any) {
+  response.send("Please go to bramagrams.com");
+});
+
+// Starts the server.
+server.listen(5000, function() {
+  console.log('Starting server on port 5000');
+});
