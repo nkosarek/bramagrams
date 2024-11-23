3c344d18d272dc5279287fdfe48b165867531694
Nicholas Kosarek <nkosarek379@gmail.com>
2024-11-23 15:54:20 -0500

    fix: Force join game page to render dynamically

--- src/app/games/(join-game)/page.tsx
+++ src/app/games/(join-game)/page.tsx
@@ -1,5 +1,7 @@
 import { JoinGamePage } from "@/ui/join-game/JoinGamePage";
 
+export const dynamic = "force-dynamic";
+
 export default function JoinGamePageRoute() {
   return <JoinGamePage />;
 }
